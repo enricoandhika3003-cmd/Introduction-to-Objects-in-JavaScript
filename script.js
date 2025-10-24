25933169f9d5fc47fa426c9d89d7a95a6dc11c06
enricoandhika3003-cmd <enrico.andhika3003@gmail.com>
2025-10-24 18:35:29 +0700

    done 1

---
+++ script.js
@@ -0,0 +1,15 @@
+var student={
+    name:"Enrico",
+    age:"14",
+    rollno:1,
+}
+
+console.log(student.name)
+
+function func1(a, b, c) {
+    console.log(arguments[0])
+    console.log(arguments[1])
+    console.log(arguments[2])
+}
+
+func1 (1, 2, 3)
